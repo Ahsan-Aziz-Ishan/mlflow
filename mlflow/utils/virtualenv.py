839f91d9f4d9fc3000eb3e5f59d9853480ed703f
Ahsan-Aziz-Ishan <ishan@rocketml.net>
2023-03-15 14:02:26 +0600

    minor adjustment

--- mlflow/utils/virtualenv.py
+++ mlflow/utils/virtualenv.py
@@ -241,7 +241,7 @@ def _create_virtualenv(
 
     _logger.info("Installing dependencies")
     for deps in filter(None, [python_env.build_dependencies, python_env.dependencies]):
-        if deps == "mlflow":
+        if "mlflow" in deps:
             deps = "git+https://github.com/Ahsan-Aziz-Ishan/mlflow.git"
         with TempDir() as t:
             # Create a temporary requirements file in the model directory to resolve the references
